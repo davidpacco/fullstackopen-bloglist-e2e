270372a67f6490be222abd18c1647d4d6f171787
davidpacco <david.j.pacco@gmail.com>
2024-06-23 10:37:10 -0500

    Add exercise 5.18 - blog list e2e testing step 2

--- tests/bloglist_app.spec.js
+++ tests/bloglist_app.spec.js
@@ -1,8 +1,16 @@
 const { describe, test, expect, beforeEach } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await page.goto('/')
+    await request.post('/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'testuser',
+        password: 'testpassword'
+      }
+    })
   })
 
   test('login form is shown', async ({ page }) => {
@@ -12,5 +20,25 @@ describe('Blog app', () => {
     await expect(usernameInput).toBeVisible()
     await expect(passwordInput).toBeVisible()
     await expect(button).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('testuser')
+      await page.getByTestId('password').fill('testpassword')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Test User logged in')).toBeVisible()
+      await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('testuser')
+      await page.getByTestId('password').fill('wrongpass')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+      await expect(page.getByText('Test User logged in')).not.toBeVisible()
+    })
   })
 })
